d7d72308cf00ceffa023cce11deb2beb50130779
prasanna <proplayer14102004@gmail.com>
2026-01-01 13:16:47 +0530

    deployment config added

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -3,5 +3,6 @@ dotenv.config();
 export const ENV ={
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV
     //seems like manager of env variables
 }
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,11 @@
 // const express = require('express');
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
-
 
 const app = express();
 
-
+const __dirname = path.resolve();
 // 2. not best practice to use dotenv in server.js but for demo purpose
 // console.log(process.env.PORT);   
 console.log(ENV.PORT);   
@@ -13,8 +13,19 @@ console.log(ENV.PORT);
 console.log(ENV.DB_URL);   
 
 // 1. undefined in terminal if dotenv not configured properly
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "success from ass" });
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: "api is up and running" });
 });
+app.get('/books', (req, res) => {
+    res.status(200).json({ message: "this is the books endpoint" });
+});
+// make ready for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../Frontend', "dist" ,"index.html"));
+    });
+}
 
 app.listen(ENV.PORT,() => console.log(`Server running on port ${ENV.PORT}`));
